d21f3e8db9f436132daca5ca3a5c277a94b62ba4
xu.wang <129871302+Asplla@users.noreply.github.com>
2023-12-12 02:20:21 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Xinxin Service',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2161448-bf3ca2b003d47389409285c7',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://www.xinxin2021.tk'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/xinxin2021'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.xinxin2021.tk'
+    },
+  ],
+};
