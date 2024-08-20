de0bb664e5a8a1ab2db963dc9433760bfaa3aa8a
Asplla <wangxu_cn@icloud.com>
2024-08-20 16:20:27 +0800

    update logos

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,7 +25,7 @@ const router = createRouter({
 
 router.beforeEach((to,from,next)=>{//beforeEach是router的钩子函数，在进入路由前执行
   if(to.meta.title){//判断是否有标题
-      document.title = to.meta.title
+    document.title = String(to.meta.title)
   }
   next()  //执行进入路由，如果不写就不会进入目标页
 })
