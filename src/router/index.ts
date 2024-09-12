2d84c79d5e6c5a5966f5d8e552c5accf75edf8e3
Asplla <wangxu_cn@icloud.com>
2024-09-12 20:48:39 +0800

    delete worker

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,8 +19,7 @@ const routes = [
   }
 ]
 const router = createRouter({
-  //history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
